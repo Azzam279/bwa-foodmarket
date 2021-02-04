adbad00a14abbdd7d36d5258f00e9cfe71ed4f3d
Azzam279 <muhammad.azzam2579@gmail.com>
2021-02-05 00:11:01 +0800

    Integrasi Flutter App & API

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
 import 'package:bwa_foodmarket/models/models.dart';
 
 part 'user_services.dart';
 part 'food_services.dart';
 part 'transaction_services.dart';
+
+String baseURL = "http://192.168.100.19/bwa-foodmarket-backend/public/api/";
